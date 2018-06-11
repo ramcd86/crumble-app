77445ff4f4ce33291ded95e0ab23c49460671f9f
Ross McDermott <ra_mcd@yahoo.com>
2018-06-11 14:30:50 +0100

    Development of menu elements.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public navStatus: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.navStatus = false;
+  }
+
+  public toggleNav(): boolean {
+    this.navStatus = !this.navStatus;
+    return false;
+  }
+
 }
