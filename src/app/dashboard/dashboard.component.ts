363f4c36c16d462cdda49500348ad3064049969c
@McDermott <ra_mcd@yahoo.com>
2018-06-11 22:29:52 +0100

    Ongoing work.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,18 +8,35 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+
   public totalSyns = 25;
   public userSyns: number;
   public dietDeficitType = 'Points';
+  public paddingSetter: any;
+
+  // public padding: number;
 
 
-  constructor() {
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
+
   }
 
   ngOnInit() {
     this.userSyns = 0;
+    document.getElementById('littleCrumb').style.paddingTop = this.paddApplier() + 'px';
+    // this.paddApplier();
+    // this.binder();
   }
 
+  public paddApplier() {
+    return (document.getElementById('outerDashContainer').clientHeight - document.getElementById('littleCrumb').clientHeight);
+  }
+
+  public binder() {
+    document.getElementById('littleCrumb').style.paddingTop = this.paddingSetter;
+  }
 
   public calculatorFunc() {
     const calCap = this.totalSyns - this.userSyns;
