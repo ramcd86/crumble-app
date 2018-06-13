904cc8512375f94d330c0b806f438cc3fbea7aff
Ross McDermott <ra_mcd@yahoo.com>
2018-06-13 12:07:55 +0100

    Building of mock back-end services.

---
+++ src/app/_constants/environments.ts
@@ -0,0 +1,6 @@
+// export class EnvironmentEndpoints {
+//   public ENDPOINT = 'http://localhost:4200/';
+// }
+export const EnvironmentEndpoints = {
+  ENDPOINT : 'http://localhost:4200/'
+};
--- src/app/_services/http/HttpServiceCore.ts
+++ src/app/_services/http/HttpServiceCore.ts
@@ -1,19 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import {EnvironmentEndpoints} from '../../_constants/environments';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HttpServiceCore {
 
-  public mockServiceBaseUrl = 'http://localhost:4200';
+  public endpoint = EnvironmentEndpoints.ENDPOINT;
 
   constructor(
     private http: HttpClient) {
   }
 
   public getMock(): Observable<any> {
-    return this.http.get(this.mockServiceBaseUrl).map(
+    return this.http.get(this.endpoint).map(
       (res: Response) => res.json()
     );
   }
