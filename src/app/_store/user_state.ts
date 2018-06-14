0ae6ae6f1ed6f6df1b221c70f447cd1cbc5d0a6e
Ross McDermott <ra_mcd@yahoo.com>
2018-06-14 14:24:24 +0100

    Modal registration system.

--- src/app/_store/user_state.ts
+++ src/app/_store/user_state.ts
@@ -1,7 +1,10 @@
 
 export class UserState {
 
-public DATA_ID: number;
-public USER_NAME: string;
+  public DATA_ID: number;
+  public USER_NAME: string;
+
+// public DATA_ID = 2;
+// public USER_NAME = 'RossMcDermott';
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
 
   public navStatus: boolean;
   public windowDesktop = false;
+  public userPresent = false;
 
   constructor(
     public userState: UserState
@@ -18,13 +19,18 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
 
-    //DEV
-    console.log(this.userState.DATA_ID, this.userState.USER_NAME);
-if (this.userState === {}) {
-  console.log('undefined');
-} else {
-  console.log('defined')
-}
+    if (this.userState.DATA_ID && this.userState.USER_NAME) {
+      this.userPresent = true;
+    } else {
+      this.userPresent = false;
+    }
+
+//     console.log(this.userState.DATA_ID, this.userState.USER_NAME);
+// if (this.userState === {}) {
+//   console.log('undefined');
+// } else {
+//   console.log('defined')
+// }
 
     this.navStatus = false;
     if (window.innerWidth < 767) {
