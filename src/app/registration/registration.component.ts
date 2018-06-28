e6acfbafd4254d53683a5f9b09a603a3c9fc15a9
Ross McDermott <ra_mcd@yahoo.com>
2018-06-28 11:23:54 +0100

    Ongoing registration construction.

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -22,6 +22,9 @@ export class RegistrationComponent implements OnInit {
   public newUserDietData: IUserDietData;
 
   public switchTemp = false;
+  public crumb3Toggle = false;
+  public crumb4Toggle = false;
+  public crumb5Toggle = false;
 
   public dbState: any;
   public validDetails = true;
@@ -51,36 +54,36 @@ export class RegistrationComponent implements OnInit {
   public bigCrumbHistory = [];
 
   public littleCrumb1Custom = true;
-  public littleCrumb1CustomType: FormControl = new FormControl(null);
-  public littleCrumb1CutomMaxValue: FormControl = new FormControl(null, [Validators.max(99999)]);
+  public littleCrumb1CustomType: FormControl = new FormControl(null, [Validators.required]);
+  public littleCrumb1CutomMaxValue: FormControl = new FormControl(null, [Validators.required, Validators.max(99999)]);
   public littleCrumb1UserSetValue = 0;
   public littleCrumb1Default = 0;
   public littleCrumb1History = [];
 
   public littleCrumb2Custom = true;
-  public littleCrumb2CustomType: FormControl = new FormControl(null);
-  public littleCrumb2CutomMaxValue: FormControl = new FormControl(null, [Validators.max(99999)]);
+  public littleCrumb2CustomType: FormControl = new FormControl(null,[Validators.required]);
+  public littleCrumb2CutomMaxValue: FormControl = new FormControl(null, [Validators.required, Validators.max(99999)]);
   public littleCrumb2UserSetValue = 0;
   public littleCrumb2Default = 0;
   public littleCrumb2History = [];
 
   public littleCrumb3Custom = false;
-  public littleCrumb3CustomType: FormControl = new FormControl(null);
-  public littleCrumb3CutomMaxValue: FormControl = new FormControl(null, [Validators.max(99999)]);
+  public littleCrumb3CustomType: FormControl = new FormControl('Points');
+  public littleCrumb3CutomMaxValue: FormControl = new FormControl(0, [Validators.max(99999)]);
   public littleCrumb3UserSetValue = 0;
   public littleCrumb3Default = 0;
   public littleCrumb3History = [];
 
   public littleCrumb4Custom = false;
-  public littleCrumb4CustomType: FormControl = new FormControl(null);
-  public littleCrumb4CutomMaxValue: FormControl = new FormControl(null, [Validators.max(99999)]);
+  public littleCrumb4CustomType: FormControl = new FormControl('Points');
+  public littleCrumb4CutomMaxValue: FormControl = new FormControl(0, [Validators.max(99999)]);
   public littleCrumb4UserSetValue = 0;
   public littleCrumb4Default = 0;
   public littleCrumb4History = [];
 
   public littleCrumb5Custom = false;
-  public littleCrumb5CustomType: FormControl = new FormControl(null);
-  public littleCrumb5CutomMaxValue: FormControl = new FormControl(null, [Validators.max(99999)]);
+  public littleCrumb5CustomType: FormControl = new FormControl('Points');
+  public littleCrumb5CutomMaxValue: FormControl = new FormControl(0, [Validators.max(99999)]);
   public littleCrumb5UserSetValue = 0;
   public littleCrumb5Default = 0;
   public littleCrumb5History = [];
@@ -108,6 +111,21 @@ export class RegistrationComponent implements OnInit {
     this.switchTemp = !this.switchTemp;
     // window.scrollTo(0, 0);
     document.getElementById('regModal').scrollTop = 0;
+  }
+  // public crumb3Toggle = false;
+  //   // public crumb4Toggle = false;
+  //   // public crumb5Toggle = false;
+  public toggleCrumb3() {
+    this.crumb3Toggle = !this.crumb3Toggle;
+    this.littleCrumb3Custom = !this.littleCrumb3Custom;
+  }
+  public toggleCrumb4() {
+    this.crumb4Toggle = !this.crumb4Toggle;
+    this.littleCrumb4Custom = !this.littleCrumb4Custom;
+  }
+  public toggleCrumb5() {
+    this.crumb5Toggle = !this.crumb5Toggle;
+    this.littleCrumb5Custom = !this.littleCrumb5Custom;
   }
 
   public logger() {
