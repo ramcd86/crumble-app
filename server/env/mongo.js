da33437a0fc974bc450d7fc15285e7c57a4c5139
Ross McDermott <ra_mcd@yahoo.com>
2018-07-11 15:50:38 +0100

    Local DB development modifications.

--- server/env/mongo.js
+++ server/env/mongo.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const encodedPw = encodeURIComponent("C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
-const mongoUri = `mongodb://localhost:${encodedPw}@localhost:10255/admin?ssl=true`;
-// const mongoUri = `mongodb://crumbsdb:oKeJmDZ77PCKzt80sMdJXu1OR1r2ahSnl7U9bdAMhXs7zwgwCEaZDrCgujxQyABpvGHbLyAOH6bs8fBIHyKInw%3D%3D@crumbsdb.documents.azure.com:10255/?ssl=true`;
+const mongoUri = `mongodb://localhost:27017`;
 function connect () {
   mongoose.set('debug', true);
   return mongoose.connect(mongoUri);
 }
+
 module.exports = {
   connect,
   mongoose
