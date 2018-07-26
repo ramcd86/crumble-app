1a0c53efecadd613bf5fd5080fb1aba4e99a6ad9
Ross McDermott <ra_mcd@yahoo.com>
2018-07-26 10:24:23 +0100

    Ongoing work.

--- src/app/diet-log/diet-log.component.ts
+++ src/app/diet-log/diet-log.component.ts
@@ -10,6 +10,9 @@ import {IUserDetails} from '../_interfaces/IUserDetails';
 export class DietLogComponent implements OnInit {
 
   public userDetails = <IUserDetails>{};
+  public array = ['DATE1_@_BLOG1', 'DATE2_@_BLOG2',  'DATE3_@_BLOG3',  'DATE4_@_BLOG4'];
+  public dateHolder = [];
+  public blogHolder = [];
 
   constructor(
     private titleService: Title,
@@ -20,6 +23,19 @@ export class DietLogComponent implements OnInit {
   ngOnInit() {
     this.titleService.setTitle('Crumbs - Diet Log');
     this.userDetails = this.session.getUserDetails();
+    this.splitBlog();
+  }
+
+  public splitBlog() {
+    let holderArray = this.array;
+    for (let i = 0; i < holderArray.length; i ++) {
+    let splitArray = [];
+    splitArray = holderArray[i].split('_@_');
+    this.dateHolder.push(splitArray[0]);
+    this.blogHolder.push(splitArray[1]);
+    }
+    console.log('dateholder: ', this.dateHolder);
+    console.log('blogholder: ', this.blogHolder);
   }
 
 }
